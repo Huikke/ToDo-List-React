62233eae493bd0a1d0ff8c2fc40e9fac6fed8f66
Lassi Chen <lachen@utu.fi>
2024-08-26 06:34:05 +0300

    checkbox states are now being saved

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,16 @@ const App = () => {
 
   const createEntry = () => {
       const entryData = {
+          id: crypto.randomUUID(),
           state: false,
           title: textboxValue,
           subentries: [],
           content: "",
-          fav: false,
-          c_time: new Date().toISOString(),
-          m_time: null,
-          m_time_content: null,
-          f_time: null,
+          favourite: false,
+          creation_time: new Date().toISOString(),
+          modification_time: null,
+          modification_time_content: null,
+          completion_time: null,
       }
 
       setEntries([...entries, entryData])
@@ -25,13 +26,31 @@ const App = () => {
       setTextboxValue("")
   }
 
+  const updateState = (entryId, newState) => {
+    const newEntries = [...entries]
+    const entryIndex = newEntries.findIndex((entry) => {
+      return entry.id == entryId
+    })
+
+    newEntries[entryIndex].state = newState
+    if (newState == true) {
+      newEntries[entryIndex].completion_time = new Date().toISOString()
+    } else {
+      newEntries[entryIndex].completion_time = null
+    }
+    console.log(entryId, newState, newEntries)
+
+    setEntries(newEntries)
+    localStorage.setItem("data", JSON.stringify(newEntries))
+  }
+
   return (
     <main>
 		  <h1>To Do List</h1>
         <div id="entries">
           {entries.map(entry => (
             <p>
-              <input type="checkbox" />
+              <input id={entry.id} type="checkbox"onChange={() => updateState(entry.id, !entry.state)} checked={entry.state}/>
               {entry.title}
             </p>
           ))}
